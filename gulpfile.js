46f156d57c3c1edf7cc9f483df538393b8070abe
xesam <xesamguo@gmail.com>
2019-12-02 15:03:20 +0800

    npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 const {src, dest} = require('gulp');
+const babel = require('gulp-babel');
 
 function copy() {
     return src('src/*.js')
-        .pipe(dest('output/'));
+        .pipe(dest('dist/'));
 }
+
+function es5() {
+    return src('src/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(dest('dist/'));
+}
+
+exports.es5 = es5;
 
 exports.default = copy;
